5d5a3e25f7f2b4918487d5dd32a20e02b050e280
Ian Ozsvald <ian@ianozsvald.com>
2014-07-14 11:30:37 +0100

    update

--- ipython_memory_usage.py
+++ ipython_memory_usage.py
@@ -2,19 +2,21 @@ import memory_profiler
 import time
 import threading
 import datetime
+import sys
 
 # %run -i memory_watcher.py
 # it'll run as a separate thread, writing to the log file
 
-WATCHER_FILENAME = "memory_usage.log"
+#WATCHER_FILENAME = "memory_usage.log"
 
 # problems:
 # long running operations (e.g. np.ones(1e8)) which take a fraction of a second
 # to complete might be reported on whilst memory is still growing...
 
 def watch_memory():
-    with open(WATCHER_FILENAME, 'w') as f:
-        f.write("Memory logger (very hacked together!) - \ntime, mem delta, mem usage, cmd\n")
+    #with open(WATCHER_FILENAME, 'w') as f:
+    #    f.write("Memory logger (very hacked together!) - \ntime, mem delta, mem usage, cmd\n")
+    print "Memory logger (very hacked together!) - \ntime, mem delta, mem usage, cmd\n"
     nbr_commands = len(In)
     memory_usage = memory_profiler.memory_usage()[0]
     while True:
@@ -30,15 +32,19 @@ def watch_memory():
                 new_cmd = True
                 nbr_commands = len(In)
             output = datetime.datetime.now(), memory_delta, new_memory_usage, In[nbr_commands-1]  # , In[nbr_commands]
-            with open(WATCHER_FILENAME, 'a') as f:
-                if new_cmd:
-                    f.write("\n")
-                f.write(str(output) + "\n")
+            #with open(WATCHER_FILENAME, 'a') as f:
+            #    if new_cmd:
+            #        f.write("\n")
+            #    f.write(str(output) + "\n")
+            if new_cmd:
+                print
+            print str(output)
+            sys.stdout.flush()
             memory_usage = new_memory_usage
         time.sleep(0.1)
 
 if __name__ == "__main__":
-    print "Memory watcher started, writing log to:", WATCHER_FILENAME
+    print "Memory watcher started"  # , writing log to:", WATCHER_FILENAME
     memory_watcher_thread = threading.Thread(target=watch_memory, args = ())
     memory_watcher_thread.daemon = True
     memory_watcher_thread.start()
